c6b279e7e7a1e90341f3523263b515b5189f29d0
hkvxkzz5jp-svg <hkvxkzz5jp@privaterelay.appleid.com>
2026-01-06 00:35:00 +1100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,11 +1,20 @@
 const { addonBuilder, getRouter } = require('stremio-addon-sdk');
-const fetch = require('node-fetch');
+
+// We are hardcoding the most popular ones so it NEVER loads empty
+const sportsChannels = [
+    { id: 'ausports_kayo_1', name: 'Kayo Sports 1', url: 'https://i.mjh.nz/au/Sydney/tv.kayo.1.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.kayo.1.png' },
+    { id: 'ausports_fox_501', name: 'Fox Sports 501', url: 'https://i.mjh.nz/au/Sydney/tv.fox.501.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.fox.501.png' },
+    { id: 'ausports_fox_league', name: 'Fox League', url: 'https://i.mjh.nz/au/Sydney/tv.fox.league.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.fox.league.png' },
+    { id: 'ausports_fox_footy', name: 'Fox Footy', url: 'https://i.mjh.nz/au/Sydney/tv.fox.footy.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.fox.footy.png' },
+    { id: 'ausports_racing_com', name: 'Racing.com', url: 'https://i.mjh.nz/au/Sydney/tv.racing.com.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.racing.com.png' },
+    { id: 'ausports_seven_sport', name: '7Sport', url: 'https://i.mjh.nz/au/Sydney/tv.7sport.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.7sport.png' }
+];
 
 const manifest = {
-    id: 'com.au.sports.fixed.final',
-    version: '1.5.0',
-    name: 'AU Sports Live',
-    description: 'Australian Sports Channels',
+    id: 'com.au.sports.instant',
+    version: '2.0.0',
+    name: 'AU Sports Instant',
+    description: 'Direct AU Sports (Hardcoded Fix)',
     resources: ['catalog', 'stream'],
     types: ['tv'],
     idPrefixes: ['ausports_'],
@@ -14,60 +23,29 @@ const manifest = {
 
 const builder = new addonBuilder(manifest);
 
-builder.defineCatalogHandler(async (args) => {
-    try {
-        const res = await fetch('https://i.mjh.nz/au/Sydney/raw.json');
-        const data = await res.json();
-        
-        // Convert the object into a list Stremio understands
-        const metas = Object.keys(data.channels)
-            .filter(key => {
-                const name = data.channels[key].name.toLowerCase();
-                return name.includes('sport') || name.includes('fox') || name.includes('kayo');
-            })
-            .map(key => ({
-                id: `ausports_${key}`,
-                type: 'tv',
-                name: data.channels[key].name,
-                poster: data.channels[key].logo,
-                posterShape: 'square'
-            }));
-
-        return { metas };
-    } catch (e) {
-        return { metas: [] };
-    }
+builder.defineCatalogHandler(() => {
+    const metas = sportsChannels.map(ch => ({
+        id: ch.id,
+        type: 'tv',
+        name: ch.name,
+        poster: ch.logo,
+        posterShape: 'square'
+    }));
+    return Promise.resolve({ metas });
 });
 
-builder.defineStreamHandler(async (args) => {
-    try {
-        const res = await fetch('https://i.mjh.nz/au/Sydney/raw.json');
-        const data = await res.json();
-        const id = args.id.replace('ausports_', '');
-        const ch = data.channels[id];
-        return { streams: ch ? [{ url: ch.url, title: 'Live Stream' }] : [] };
-    } catch (e) {
-        return { streams: [] };
-    }
+builder.defineStreamHandler((args) => {
+    const channel = sportsChannels.find(ch => ch.id === args.id);
+    return Promise.resolve({ 
+        streams: channel ? [{ url: channel.url, title: 'Live Stream' }] : [] 
+    });
 });
 
-const addonInterface = builder.getInterface();
-const router = getRouter(addonInterface);
+const router = getRouter(builder.getInterface());
 
 module.exports = (req, res) => {
-    // These 3 lines are the "magic" for Omni/Stremio
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-
-    if (req.method === 'OPTIONS') {
-        res.writeHead(204);
-        res.end();
-        return;
-    }
-
-    router(req, res, () => {
-        res.statusCode = 404;
-        res.end();
-    });
+    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Content-Type', 'application/json');
+    router(req, res, () => { res.status(404).send('Not found'); });
 };
