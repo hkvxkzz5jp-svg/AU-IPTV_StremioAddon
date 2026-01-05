9d2f68edf94b156aa3b5c05e82794fe48b93db19
hkvxkzz5jp-svg <hkvxkzz5jp@privaterelay.appleid.com>
2026-01-06 00:39:56 +1100

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,21 +1,18 @@
 const { addonBuilder, getRouter } = require('stremio-addon-sdk');
 
-// We are hardcoding the most popular ones so it NEVER loads empty
 const sportsChannels = [
-    { id: 'ausports_kayo_1', name: 'Kayo Sports 1', url: 'https://i.mjh.nz/au/Sydney/tv.kayo.1.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.kayo.1.png' },
-    { id: 'ausports_fox_501', name: 'Fox Sports 501', url: 'https://i.mjh.nz/au/Sydney/tv.fox.501.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.fox.501.png' },
-    { id: 'ausports_fox_league', name: 'Fox League', url: 'https://i.mjh.nz/au/Sydney/tv.fox.league.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.fox.league.png' },
-    { id: 'ausports_fox_footy', name: 'Fox Footy', url: 'https://i.mjh.nz/au/Sydney/tv.fox.footy.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.fox.footy.png' },
-    { id: 'ausports_racing_com', name: 'Racing.com', url: 'https://i.mjh.nz/au/Sydney/tv.racing.com.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.racing.com.png' },
-    { id: 'ausports_seven_sport', name: '7Sport', url: 'https://i.mjh.nz/au/Sydney/tv.7sport.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.7sport.png' }
+    { id: 'ausports_kayo1', name: 'Kayo Sports 1', url: 'https://i.mjh.nz/au/Sydney/tv.kayo.1.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.kayo.1.png' },
+    { id: 'ausports_fox501', name: 'Fox Sports 501', url: 'https://i.mjh.nz/au/Sydney/tv.fox.501.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.fox.501.png' },
+    { id: 'ausports_foxleague', name: 'Fox League', url: 'https://i.mjh.nz/au/Sydney/tv.fox.league.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.fox.league.png' },
+    { id: 'ausports_foxfooty', name: 'Fox Footy', url: 'https://i.mjh.nz/au/Sydney/tv.fox.footy.m3u8', logo: 'https://i.mjh.nz/au/Sydney/tv.fox.footy.png' }
 ];
 
 const manifest = {
-    id: 'com.au.sports.instant',
-    version: '2.0.0',
-    name: 'AU Sports Instant',
-    description: 'Direct AU Sports (Hardcoded Fix)',
-    resources: ['catalog', 'stream'],
+    id: 'com.au.sports.fixed.playback',
+    version: '2.5.0',
+    name: 'AU Sports Live',
+    description: 'Live AU Sports (Fixed Playback)',
+    resources: ['catalog', 'meta', 'stream'],
     types: ['tv'],
     idPrefixes: ['ausports_'],
     catalogs: [{ type: 'tv', id: 'ausports_cat', name: 'AU Sports' }]
@@ -24,28 +21,53 @@ const manifest = {
 const builder = new addonBuilder(manifest);
 
 builder.defineCatalogHandler(() => {
-    const metas = sportsChannels.map(ch => ({
-        id: ch.id,
-        type: 'tv',
-        name: ch.name,
-        poster: ch.logo,
-        posterShape: 'square'
-    }));
-    return Promise.resolve({ metas });
+    return Promise.resolve({
+        metas: sportsChannels.map(ch => ({
+            id: ch.id,
+            type: 'tv',
+            name: ch.name,
+            poster: ch.logo,
+            posterShape: 'square'
+        }))
+    });
+});
+
+builder.defineMetaHandler((args) => {
+    const ch = sportsChannels.find(c => c.id === args.id);
+    return Promise.resolve({
+        meta: ch ? { id: ch.id, type: 'tv', name: ch.name, poster: ch.logo, posterShape: 'square' } : {}
+    });
 });
 
 builder.defineStreamHandler((args) => {
-    const channel = sportsChannels.find(ch => ch.id === args.id);
-    return Promise.resolve({ 
-        streams: channel ? [{ url: channel.url, title: 'Live Stream' }] : [] 
-    });
+    const ch = sportsChannels.find(c => c.id === args.id);
+    if (ch) {
+        return Promise.resolve({
+            streams: [{
+                title: 'Direct HLS Stream',
+                url: ch.url,
+                behaviorHints: {
+                    isLive: true,
+                    notWebReady: false // Tells Stremio it's okay for web/app players
+                }
+            }]
+        });
+    }
+    return Promise.resolve({ streams: [] });
 });
 
 const router = getRouter(builder.getInterface());
 
 module.exports = (req, res) => {
+    // ESSENTIAL: These headers tell Omni it's safe to play the video
     res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, OPTIONS');
     res.setHeader('Access-Control-Allow-Headers', '*');
-    res.setHeader('Content-Type', 'application/json');
+    
+    if (req.method === 'OPTIONS') {
+        res.status(204).end();
+        return;
+    }
+
     router(req, res, () => { res.status(404).send('Not found'); });
 };
